683c31cac9b2fafec7bf5fc18e56d6c151016db7
JoseMRLC <josemriveiroticbgmail.com>
2024-09-26 13:54:12 +0200

    ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            throw new Exception("Error en el main");
+        } catch (Exception e) {
+            System.out.println("La excepcion es: " + e.getMessage());
+        }
     }
 }
